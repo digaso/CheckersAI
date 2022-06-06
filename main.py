70664a61e7cc3129e29cb2d775424839ddac5f3d
digaso <diogomoura2014@gmail.com>
2022-06-06 16:31:52 +0100

    print checkers boards

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
-from  checkersboard.constants import WIDTH, HEIGHT
+from checkersboard.constants import WIDTH, HEIGHT
+from checkersboard.board import Board
+import pyswip as prolog
 
 FPS = 60
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
+
 def main():
+
+    board = Board()
     run = True
     clock = pygame.time.Clock()
 
@@ -14,5 +19,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+        board.draw(SCREEN)
+        pygame.display.update()
     pygame.quit()
+
+
 main()
